ebc810471b0fb1dae52885985666b849f901c6a7
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2022-08-23 21:18:19 +0800

    manu（all）

--- Assets/Scripts/Gamemanu.cs
+++ Assets/Scripts/Gamemanu.cs
@@ -5,13 +5,45 @@ using UnityEngine;
 public class Gamemanu : MonoBehaviour
 {
     public GameObject pauseMenu;
-
+    public GameObject optionMenu;
+    public GameObject loadMenu;
+    //暂停调出菜单
     public void PauseGame()
     {
+        Time.timeScale = 0f;
         pauseMenu.SetActive(true);
     }
+    //回到游戏
     public void ResumeGame()
     {
+        Time.timeScale = 1f;
         pauseMenu.SetActive(false);
     }
+    //设置界面开关
+    public void SetOption()
+    {
+        pauseMenu.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+
+    public void CloseOption()
+    {
+        optionMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+        
+    }
+    //读档界面开关
+    public void SetLoad()
+    {
+        pauseMenu.SetActive(false);
+        loadMenu.SetActive(true);
+    }
+
+    public void CloseLoad()
+    {
+        pauseMenu.SetActive(true);
+        loadMenu.SetActive(false);
+
+    }
+
 }
