73173d8b42d9462d42e56cb161f50c1c420663b9
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2022-08-23 14:53:34 +0800

    pausemenu pop out

---
+++ Assets/Scripts/Gamemanu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+    }
+}
