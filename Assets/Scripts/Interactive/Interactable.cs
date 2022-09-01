7639b1807b9f2a41b5dc6bb4acea5922e837829b
RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
2022-09-01 02:56:03 -0700

    Squashed commit of the following:
    commit a02d7908289d47bbce96fbcc9035e433dae1957a
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Thu Sep 1 02:55:24 2022 -0700
    
        Hiding control update
    
    commit 10cdd6890627fedd98e18974c3b51908c6aaf105
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 31 02:54:25 2022 -0700
    
        button
    
    commit efe4d714571654feeb579ae5dac2c14e25c3a581
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 31 01:13:05 2022 -0700
    
        Added interaction button
    
    commit d2e3fcf882eff6e52d7446e045c7fa9c46f0e06b
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Tue Aug 30 01:49:43 2022 -0700
    
        FSM update and Invisi Walls
    
    commit f46da0098caac4d4840d4a9e4e54a5dbc85603a0
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Tue Aug 30 01:49:20 2022 -0700
    
        FSM update + Invisi Walls
    
    commit 696bd7031bcc5fe280899bdec90aa8ddcba4c01e
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 18:17:31 2022 -0700
    
        speed
    
    commit 9a862dadb7b585496dea39f7ee919b0222ad3f1a
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 18:12:09 2022 -0700
    
        fixed FSM and tag
    
    commit edd7bf335d65dd9a41fadb991e528ef338e8d698
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:45:19 2022 -0700
    
        Updated Detection boxes
    
    commit 73e61eceef0e1d24cb87504c995024fb744bb899
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:34:00 2022 -0700
    
        Revert "Revert "Squashed commit of the following:""
    
        This reverts commit 6f7fc9fa526e26a0ece1117409e884f594e5d041.
    
    commit 0dc66c086ef050d6261d40a23a3a4dbc17d31c39
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:33:25 2022 -0700
    
        Revert "Update SampleScene.unity"
    
        This reverts commit 9be69a59367ee3de31a6ffbb008880596bdfbf3e.
    
    commit 6f7fc9fa526e26a0ece1117409e884f594e5d041
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:32:56 2022 -0700
    
        Revert "Squashed commit of the following:"
    
        This reverts commit 23635f80d774fd709786888fd3e9bc7edf682301.
    
    commit 23635f80d774fd709786888fd3e9bc7edf682301
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:22:07 2022 -0700
    
        Squashed commit of the following:
    
        commit bfa39d5f1f2a6e99a210f99e4a83e92afdcf9d53
        Author: canonindd <110882912+canonindd@users.noreply.github.com>
        Date:   Wed Aug 24 20:20:51 2022 +0800
    
            删除单个eventsystem&canvas (#11)
    
    commit 9be69a59367ee3de31a6ffbb008880596bdfbf3e
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:21:20 2022 -0700
    
        Update SampleScene.unity
    
    commit b9fdbf4eec87f217cd702e8674d76119aacfcf27
    Author: RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
    Date:   Wed Aug 24 05:09:46 2022 -0700
    
        Updated FSM

---
+++ Assets/Scripts/Interactive/Interactable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRnge;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/charcontroller.cs
+++ Assets/Scripts/charcontroller.cs
@@ -1,27 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class charcontroller : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
     public Joystick joystick;
+    private bool canHide = false;
+    private bool hiding = false;
+    private Button interact;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        interact = GameObject.Find("Interact").GetComponent<Button>();
+
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
         GroundMovement();
+
+        if (!hiding)
+        {
+            speed = 100;
+        }
+        else
+        {
+            speed = 0;
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Hidable"))
+        {
+            canHide = true;
+        }
     }
 
-
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Hidable"))
+        {
+            canHide = false;
+        }
+    }
     void GroundMovement() 
     {
         float horizontalmove = joystick.Horizontal;
@@ -34,6 +63,21 @@ public class charcontroller : MonoBehaviour
         if (horizontalmove < 0f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+    public void hide()
+    {
+        if (hiding == true)
+        {
+            Physics2D.IgnoreLayerCollision(6, 7, false);
+            Physics2D.IgnoreLayerCollision(6, 8, false);
+            hiding = false;
+        }
+        else if (canHide)
+        {
+            Physics2D.IgnoreLayerCollision(6, 7, true);
+            Physics2D.IgnoreLayerCollision(6, 8, true);
+            hiding = true;
         }
     }
 }
